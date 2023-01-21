234305c35f02b9c70c33e2301472c00bd0dbae10
fufuyuyu <123218298+fufuyuyu@users.noreply.github.com>
2023-01-21 11:28:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ow-cv5mVfVEtceUT941wZwleCCd8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '9i4PS0t02T9d9WZNADOovEWXmZiWbHeUou_BFplsQfk',
+      useTemplateId: 'zOfNJ7fvav_273YFHLvLUV3di8SjnAau_Db5HaHYZFQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-26',
       festivals: [
@@ -47,7 +47,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '9i4PS0t02T9d9WZNADOovEWXmZiWbHeUou_BFplsQfk',
+  CALLBACK_TEMPLATE_ID: 'zOfNJ7fvav_273YFHLvLUV3di8SjnAau_Db5HaHYZFQ',
 
   CALLBACK_USERS: [
     {
